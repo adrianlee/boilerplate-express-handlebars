b9b185ac06d9d11d5c6e375873ab4de37896b46a
Adrian Lee <jun.irok@gmail.com>
2012-08-05 02:22:46 -0400

    express + hbs + boostrap

---
+++ app.js
@@ -0,0 +1,65 @@
+var express = require('express'),
+    routes = require('./routes'),
+    http = require('http'),
+    path = require('path'),
+
+    // Third Party
+    hbs = require('hbs');
+
+var app = express();
+
+////////////////////////////////////////////////
+// Express Configuration
+////////////////////////////////////////////////
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'html');
+  app.engine('html', require('hbs').__express);
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+////////////////////////////////////////////////
+// Handlebars
+////////////////////////////////////////////////
+var blocks = {};
+
+hbs.registerHelper('extend', function(name, context) {
+    var block = blocks[name];
+    if (!block) {
+        block = blocks[name] = [];
+    }
+
+    block.push(context(this));
+});
+
+hbs.registerHelper('block', function(name) {
+    var val = (blocks[name] || []).join('\n');
+
+    // clear the block
+    blocks[name] = [];
+    return val;
+});
+
+////////////////////////////////////////////////
+// Router
+////////////////////////////////////////////////
+app.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+////////////////////////////////////////////////
+// HTTP Server
+////////////////////////////////////////////////
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
